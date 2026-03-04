ad9710534837ef5d964b773324c8af6bfc36edcb
Mohamed Amr <126739293+Mohamed-F22@users.noreply.github.com>
2026-03-05 00:02:13 +0200

    ..

--- src/app/AOS.tsx
+++ src/app/AOS.tsx
@@ -2,13 +2,18 @@
 
 import { useEffect } from 'react';
 import AOS from 'aos';
+import { usePathname } from 'next/navigation';
 
 export default function AOSInit() {
+  const pathname = usePathname();   
   useEffect(() => {
     AOS.init({
       duration: 1000,
       once: true,
     });
   }, []);
+  useEffect(() => {
+    AOS.refresh(); 
+  }, [pathname]);
   return null;
 }
