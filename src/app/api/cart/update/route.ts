8fe6af4e66d996661d3a8634fbb554e4f9438bb7
Mohamed Amr <126739293+Mohamed-F22@users.noreply.github.com>
2026-03-13 08:42:40 +0200

    update cart endpoint

---
+++ src/app/api/cart/update/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import { dbConnect } from "@/app/lib/mongodb";
+import jwt from "jsonwebtoken";
+import { cookies } from "next/headers";
+import { updateItemInCart } from "./service";
+
+const SECRET = process.env.SECRET_JWT as string;
+
+export async function PATCH(req: Request) {
+  try {
+    await dbConnect();
+
+    // 1. التحقق من التوكن
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token")?.value;
+
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const decoded = jwt.verify(token, SECRET) as { id: string };
+    const userId = decoded.id;
+
+    const { productId, size, quantity } = await req.json();
+
+    const cart = await updateItemInCart({ productId, size, quantity, userId });
+
+    return NextResponse.json(
+      {
+        message: "Cart updated successfully",
+        cart,
+      },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.error("Update cart error:", err);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
